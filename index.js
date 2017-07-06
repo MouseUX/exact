c1a15e015088aee7edabf1a7ca3f23b10291210c
idocarmi22 <cheado22@gmail.com>
2017-07-06 08:27:34 +0300

    dadsd

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ app.get('/', function(req, res) {
 app.use(lessMiddleware(public_dir,{"debug":true}));
 app.use(express.static(public_dir));
 
-app.listen(process.env.PORT || 5000);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
