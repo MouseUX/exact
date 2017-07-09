5849fc2b6b69ef91fb67d15bf69b4f40b15d71bb
Shmulik Chicvashvili <s.chicvashvili@gmail.com>
2017-07-09 13:10:03 +0300

    Added the connection between the search and the itemlist

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,20 @@
-import {Component} from '@angular/core';
-import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home',
   styleUrls: ['./home.component.css'],
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  rotuer: any;
   name: string;
-  constructor() {
-    this.name="dani"
+  constructor(private router: Router) {
+    this.router = router;
+    this.name = 'dani';
   }
-  //
-  // go() {
-  //   this.router.navigate([`../itemsList`], { relativeTo: this.route });
-  // }
 
-  sayMyName() {
-    this.name="fsaf"
-    console.log('My name is')
+  moveRoute() {
+    this.router.navigate([`/itemsList`]);
   }
 }
